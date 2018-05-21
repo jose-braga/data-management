23a831997b94b4d6bb47e294659197eb960b1989
Jose Braga <josecbraga@gmail.com>
2018-05-21 13:10:17 +0000

    corrected retry strategies on external API requests

--- app_api/config/external-api.js
+++ app_api/config/external-api.js
@@ -9,6 +9,24 @@ var baseURL = {1: 'https://www.requimte.pt/ucibio/api',
 */
 exports.baseURL = baseURL;
 
+var RETRIABLE_ERRORS = ['ECONNRESET', 'ENOTFOUND', 'ESOCKETTIMEDOUT', 'ETIMEDOUT', 'ECONNREFUSED', 'EHOSTUNREACH', 'EPIPE', 'EAI_AGAIN'];
+var _ = require('lodash');
+
+function retryBodyOrHTTPOrNetwork(err, response, body) {
+    // retry the request if: 1 - network error, 2 - HTTP error, 3 - body does not parse to JSON
+    // the network and http parts where taken from requestretry code.
+    var network_err = err && _.includes(RETRIABLE_ERRORS, err.code);
+    var http_err = (response && 500 <= response.statusCode && response.statusCode < 600);
+    var body_err;
+    try {
+        body = JSON.parse(body);
+        body_err = false;
+    } catch (error) {
+        body_err = true;
+    }
+    return network_err || http_err || body_err;
+}
+
 var contact = function (baseURL, operation, entityType, entityID, errorMessage, errorIDs) {
     // Note: request is asynchronous !
     // errorIDs are the IDs associated with the request being done
@@ -17,14 +35,15 @@ var contact = function (baseURL, operation, entityType, entityID, errorMessage, 
             url: baseURL + '/' + operation + '/' + entityType + '/' + entityID,
             maxAttempts: 5,
             retryDelay: 5000,
-            retryStrategy: request.RetryStrategies.HTTPOrNetworkError
+            retryStrategy: retryBodyOrHTTPOrNetwork
         }, function (error, response, body) {
             if (body !== undefined) {
                 if (body.length >0) {
-                    if (body[0] !== 'T') {
+                    try {
                         body = JSON.parse(body);
                     }
-                    else {
+                    catch (err) {
+                        console.log('error response:', err);
                         body = {'statusCode': 5000};
                     }
                 }
@@ -47,14 +66,15 @@ exports.contactCreateOrUpdate = function (baseURL, entityType, entityID, errorMe
             url: baseURL + '/' + 'create' + '/' + entityType + '/' + entityID,
             maxAttempts: 5,
             retryDelay: 5000,
-            retryStrategy: request.RetryStrategies.HTTPOrNetworkError
+            retryStrategy: retryBodyOrHTTPOrNetwork
         }, function (error, response, body) {
             if (body !== undefined) {
                 if (body.length >0) {
-                    if (body[0] !== 'T') {
+                    try {
                         body = JSON.parse(body);
                     }
-                    else {
+                    catch (err) {
+                        console.log('error response:', err);
                         body = {'statusCode': 5000};
                     }
                 }
